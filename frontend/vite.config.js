c8fdcf1cd504ec96f206b8c5e44777c691a87da6
fasiloffcial <muhammedfasiloffcial@gmail.com>
2024-01-15 23:56:33 +0530

    backend server url updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://mern-auth-app-server-pv1r.onrender.com",
         changeOrigin: true,
       },
     },
